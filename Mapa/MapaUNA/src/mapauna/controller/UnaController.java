2d1ca0c3bcb2694817d7d41ad0be74d684e4527c
ronald0309 <42876234+ronald0309@users.noreply.github.com>
2020-11-13 23:27:04 -0600

    se agregan radio butoms

--- Mapa/MapaUNA/src/mapauna/controller/UnaController.java
+++ Mapa/MapaUNA/src/mapauna/controller/UnaController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 /**
@@ -52,6 +53,180 @@ public class UnaController implements Initializable {
     private Button btnReiniciar;
     @FXML
     private Button btnSalir;
+    @FXML
+    private AnchorPane contMap;
+    @FXML
+    private RadioButton V1;
+    @FXML
+    private RadioButton V2;
+    @FXML
+    private RadioButton V3;
+    @FXML
+    private RadioButton V4;
+    @FXML
+    private RadioButton V5;
+    @FXML
+    private RadioButton V6;
+    @FXML
+    private RadioButton V7;
+    @FXML
+    private RadioButton V8;
+    @FXML
+    private RadioButton V9;
+    @FXML
+    private RadioButton V10;
+    @FXML
+    private RadioButton V11;
+    @FXML
+    private RadioButton V12;
+    @FXML
+    private RadioButton V13;
+    @FXML
+    private RadioButton V14;
+    @FXML
+    private RadioButton V15;
+    @FXML
+    private RadioButton V16;
+    @FXML
+    private RadioButton V17;
+    @FXML
+    private RadioButton V18;
+    @FXML
+    private RadioButton V19;
+    @FXML
+    private RadioButton V20;
+    @FXML
+    private RadioButton V21;
+    @FXML
+    private RadioButton V22;
+    @FXML
+    private RadioButton V23;
+    @FXML
+    private RadioButton V24;
+    @FXML
+    private RadioButton V25;
+    @FXML
+    private RadioButton V26;
+    @FXML
+    private RadioButton V27;
+    @FXML
+    private RadioButton V28;
+    @FXML
+    private RadioButton V29;
+    @FXML
+    private RadioButton V30;
+    @FXML
+    private RadioButton V31;
+    @FXML
+    private RadioButton V32;
+    @FXML
+    private RadioButton V33;
+    @FXML
+    private RadioButton V34;
+    @FXML
+    private RadioButton V35;
+    @FXML
+    private RadioButton V36;
+    @FXML
+    private RadioButton V37;
+    @FXML
+    private RadioButton V38;
+    @FXML
+    private RadioButton V39;
+    @FXML
+    private RadioButton V40;
+    @FXML
+    private RadioButton V41;
+    @FXML
+    private RadioButton V42;
+    @FXML
+    private RadioButton V43;
+    @FXML
+    private RadioButton V44;
+    @FXML
+    private RadioButton V45;
+    @FXML
+    private RadioButton V46;
+    @FXML
+    private RadioButton V47;
+    @FXML
+    private RadioButton V48;
+    @FXML
+    private RadioButton V49;
+    @FXML
+    private RadioButton V50;
+    @FXML
+    private RadioButton V51;
+    @FXML
+    private RadioButton V52;
+    @FXML
+    private RadioButton V53;
+    @FXML
+    private RadioButton V54;
+    @FXML
+    private RadioButton V55;
+    @FXML
+    private RadioButton V56;
+    @FXML
+    private RadioButton V57;
+    @FXML
+    private RadioButton V58;
+    @FXML
+    private RadioButton V59;
+    @FXML
+    private RadioButton V60;
+    @FXML
+    private RadioButton V61;
+    @FXML
+    private RadioButton V62;
+    @FXML
+    private RadioButton V63;
+    @FXML
+    private RadioButton V64;
+    @FXML
+    private RadioButton V65;
+    @FXML
+    private RadioButton V66;
+    @FXML
+    private RadioButton V67;
+    @FXML
+    private RadioButton V68;
+    @FXML
+    private RadioButton V69;
+    @FXML
+    private RadioButton V70;
+    @FXML
+    private RadioButton V71;
+    @FXML
+    private RadioButton V72;
+    @FXML
+    private RadioButton V73;
+    @FXML
+    private RadioButton V74;
+    @FXML
+    private RadioButton V75;
+    @FXML
+    private RadioButton V76;
+    @FXML
+    private RadioButton V77;
+    @FXML
+    private RadioButton V78;
+    @FXML
+    private RadioButton V79;
+    @FXML
+    private RadioButton V80;
+    @FXML
+    private RadioButton V81;
+    @FXML
+    private RadioButton V82;
+    @FXML
+    private RadioButton V83;
+    @FXML
+    private RadioButton V84;
+    @FXML
+    private RadioButton V85;
+    
+    
 
     /**
      * Initializes the controller class.
@@ -59,7 +234,7 @@ public class UnaController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void onActionViaIzquierda(ActionEvent event) {
@@ -94,5 +269,5 @@ public class UnaController implements Initializable {
         Stage stage = (Stage) btnInciar.getScene().getWindow();
         stage.close();
     }
-    
+
 }
